556d564f3de4dc85dc1e9e0b00f3b2c210f8fa0d
Stephenalexngo <51846517+Stephenalexngo@users.noreply.github.com>
2021-02-05 14:41:55 +0800

    added template for main

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -6,6 +6,12 @@ import java.io.FileWriter;
 import java.util.*;
 
 public class Main {
+    public static boolean DoLLOneParsing(String line){
+        
+
+        return true;
+    }
+
     public static void main(String[] args) throws Exception {
         File input_file = new File("Main/src/input.txt");
         FileReader fr = new FileReader(input_file);
@@ -14,7 +20,23 @@ public class Main {
 
         FileWriter fw = new FileWriter("Main/src/output.txt");
 
-        
+        while( (line = br.readLine()) != null){
+            if(!line.equals("")){
+                fw.append(line + " - ");
+                line = line.replace(" ", "");
+
+                if(DoLLOneParsing(line)){
+                    fw.append("ACCEPT");
+                }
+                else{
+                    fw.append("REJECT");
+                }
+                
+                fw.append("\n");
+            }
+        }
+
+        System.out.println("Parsing Complete!");
 
         fr.close();
         fw.close();
